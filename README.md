88c0815c8dcb64bf249f7724648aad7a7d377fb0
ruanjiayou <1439120442@qq.com>
2018-04-17 15:48:50 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # proxy
 正向代理请求
+```
+根据本机 C:/Windows/System32/drivers/etc/hosts 文件转发请求
+格式
+127.0.0.1       baidu.map.jiayou.com#8084
+```
+
+## 安装与运行
+```
+npm install http://github.com/ruanjiayou/proxy.git --save
+进入项目目录(默认转发到localhost)
+nodemon proxy.js 或 npm run start
+```
---
+++ proxy.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var fs = require('fs');
+var proxy = require('http-proxy').createProxyServer({});
+var config = {}, ip = 'localhost';
+
+var debug = require('debug')('proxy');
+
+(function () {
+  try {
+    var str = fs.readFileSync('C:/Windows/System32/drivers/etc/hosts');
+    var reg = /[0-9.]+\s+([0-9a-zA-Z.]+)#(\d+)([^\d]*)[\n\r]+/g;
+    var m;
+    while ((m = reg.exec(str)) !== null) {
+      config[m[1]] = m[2];
+      console.log('端口:' + m[2] + '\t域名:' + m[1]);
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
+
+proxy.on(function (err, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+});
+proxy.on('error', function (err, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain;charset=utf-8'
+  });
+  res.end(new Buffer('代理服务器错误！'));
+});
+// 另外新建一个 HTTP 80 端口的服务器，也就是常规 Node 创建 HTTP 服务器的方法。
+// 在每次请求中，调用 proxy.web(req, res config) 方法进行请求分发
+var server = http.createServer(function (req, res) {
+  var host = req.headers.host;
+  //,ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; 
+  for (var k in config) {
+    if (k === host) {
+      //console.log(req.headers);
+      proxy.web(req, res, { target: 'http://' + ip + ':' + config[k] });
+      return;
+    }
+  }
+  res.writeHead(200, {
+    'Content-Type': 'text/plain;charset=utf-8'
+  });
+  res.end('Welcome to my server!');
+});
+debug('代理服务器正在监听80端口!');
+server.listen(80);
